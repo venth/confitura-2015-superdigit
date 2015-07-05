546453b01c9d96ea8f24603c04db4a0a4b79c70b
venth <artur.krysiak@protonmail.com>
2015-07-05 16:11:42 +0200

    added tests for digitizing iterator along with the iterator; made superdigit calculation to work

--- superdigit.py
+++ superdigit.py
@@ -2,4 +2,43 @@ __author__ = 'venth'
 
 
 def calc(number):
-    return number
+
+    calculated = reduce(
+        lambda current, digit: dict(no = current['no'] + 1, sum=current['sum'] + digit),
+        _DigitizingIterator(number=number),
+        dict(no=0, sum=0),
+    )
+
+    reduced_to_one_digit = calculated['no'] == 1
+    if reduced_to_one_digit:
+        return calculated['sum']
+    else:
+        return calc(calculated['sum'])
+
+
+class _DigitizingIterator(object):
+    def __init__(self, number):
+        if number < 0:
+            self.sign = -1
+            self._number = -number
+        else:
+            self.sign = 1
+            self._number = number
+
+    def next(self):
+        digit = self._number % 10
+
+        all_digits_extracted = self._number == -1
+        if all_digits_extracted:
+            raise StopIteration
+
+        self._number /= 10
+
+        need_to_be_stopped = self._number == 0
+        if need_to_be_stopped:
+            self._number = -1
+
+        return self.sign * digit
+
+    def __iter__(self):
+        return self
--- test/superdigit_test.py
+++ test/superdigit_test.py
@@ -46,12 +46,14 @@ class SuperdigitTest(unittest.TestCase):
             dict(number=88, superdigit=7),
             dict(number=532, superdigit=1),
             dict(number=4567, superdigit=4),
+            dict(number=98741, superdigit=2),
             dict(number=-10, superdigit=-1),
             dict(number=-99, superdigit=-9),
             dict(number=-999, superdigit=-9),
             dict(number=-88, superdigit=-7),
             dict(number=-532, superdigit=-1),
             dict(number=-4567, superdigit=-4),
+            dict(number=-98741, superdigit=-2),
         ]
 
         # when super digit is calculated for every given number
@@ -70,3 +72,42 @@ class SuperdigitTest(unittest.TestCase):
 
         # then superdigit is calculated without error
         pass
+
+
+class _DigitizingIteratorTest(unittest.TestCase):
+
+    def test_for_given_positive_number_return_digits_as_were_predigitized(self):
+        # given positive numbers
+        positive_numbers = [0, 1, 100, 200, 1212312, 43543512, 123123, 4345435, 12312312, 34435435, 9234748753, sys.maxint]
+
+        for number in positive_numbers:
+            self._for_a_given_positive_number_returns_its_digit(number=number)
+
+    def test_negative_numbers_are_digitized_with_negative_digits(self):
+        # given negative numbers
+        negative_numbers = [-1, -100, -200, -1212312, -43543512, -123123, -4345435, -12312312, -34435435, -12397654512, -sys.maxint]
+
+        for number in negative_numbers:
+            self._for_a_given_negative_number_returns_its_digit(number=number)
+
+    def _for_a_given_positive_number_returns_its_digit(self, number):
+        # when the given number is digitized
+        digitized = list(superdigit._DigitizingIterator(number=number))
+
+        # then the number is digitized the same as in text made from the number
+        digits_from_number = list(itertools.imap(
+            lambda str_digit: int(str_digit),
+            str(number),
+        ))
+        self.assertItemsEqual(digits_from_number, digitized, 'Expected number: %s, was: %r' % (digits_from_number, digitized))
+
+    def _for_a_given_negative_number_returns_its_digit(self, number):
+        # when the given number is digitized
+        digitized = list(superdigit._DigitizingIterator(number=number))
+
+        # then the number is digitized with a negative digits
+        digits_from_number = list(itertools.imap(
+            lambda str_digit: -int(str_digit),
+            str(-number),
+        ))
+        self.assertItemsEqual(digits_from_number, digitized, 'Expected number: %s, was: %r' % (digits_from_number, digitized))
